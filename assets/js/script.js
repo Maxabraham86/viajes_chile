bef211bfae8e8678475c4c54435e2f08483e1a04
Maximiliano Abraham <maxabrahamproyectos@gmail.com>
2024-03-11 20:43:20 -0300

    se linkea script se realiza tooltip

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,13 @@ const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
 
+
+
+
+
+
+
+
 $(document).ready(function () {
     $("a").on('click', function (event) {
         if (this.hash !== "") {
